e8cf8cea36723e2981629a43c2261851b4a273b9
Babar Ali <137874899+BabarMalikAwan@users.noreply.github.com>
2024-09-04 19:30:07 +0500

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -29,7 +29,7 @@ function Contact() {
           </Col>
         </Row>
         <Row>
-          <Col md={6} className={styles.mapContainer}>
+          <Col md={6} xs={12} className={styles.mapContainer}>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d348665.7608735737!2d73.30084257934714!3d32.49499432669972!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391d9d7dd430e0b5%3A0x238eb3b10f7a5f88!2sSialkot%2C%20Pakistan!5e0!3m2!1sen!2sus!4v1633395443481!5m2!1sen!2sus"
               title="Google Maps"
@@ -37,7 +37,7 @@ function Contact() {
               loading="lazy"
             ></iframe>
           </Col>
-          <Col md={6} className={styles.formContainer}>
+          <Col md={6} xs={12} className={styles.formContainer}>
             <div className={styles.contactForm}>
               <h2>Get in Touch</h2>
               <form>
@@ -61,3 +61,4 @@ function Contact() {
 }
 
 export default Contact;
+
